71f187eca969d34368631413b4516996107aa41c
ryan.Rah <re.rahdari@gmail.com>
2024-06-13 14:24:27 +0200

    Add Example of 'Method'.

--- src/main/java/se/lexicon/classAndObjectExample.java
+++ src/main/java/se/lexicon/classAndObjectExample.java
@@ -10,11 +10,11 @@ public class classAndObjectExample {
             // Adding a behaviour (Methods)
 
             void Start() {
-                System.out.println("Car is Starting");
+                System.out.println("My Car with model" + " " + model + " is Starting");
             }
 
             void Stop() {
-                System.out.println("Car is Stopping");
+                System.out.println("My Car with model" + " " + model + " is Stopping");
             }
         } // Car class
 
@@ -27,12 +27,13 @@ public class classAndObjectExample {
         Mycar.model = "Tesla Model S";
         Mycar.year = 2018;
 
-        Mycar.Start();  // Outputs: Car is starting
-        Mycar.Stop();   // Outputs: Car is stopping
+        System.out.println("The model of my car is " + Mycar.model + " from " + Mycar.year + " " +
+                "and" + " the color is " + Mycar.color); // Output: The model of my car is Tesla
+        // Model S from 2018 and the color is red.
 
         // Accessing properties
+        Mycar.Start();  // Output: My Car with model Tesla Model S is Starting
+        Mycar.Stop();   // Output: My Car with model Tesla Model S is Stopping
 
-        System.out.println("The model of my car is " + Mycar.model + " from " + Mycar.year + " " +
-                "and" + " the color is " + Mycar.color);
     } //Main
 } //Class
